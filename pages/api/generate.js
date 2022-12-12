16b089f4e6560536a656605dc46264a6e5dcc901
Flip Abagnale <69315643+flip-abagnale@users.noreply.github.com>
2022-12-12 09:52:41 -0600

    initial special version

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+Write a short reply for when asked "What would Satoshi Nakamoto do in this situation?"
+
+Situation:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.9,
+    max_tokens: 400,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
